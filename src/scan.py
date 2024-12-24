8224b1edef57c297a585b2cba335ff1ce9a0e58c
Soban Shoaib <sobanshoaib7500@gmail.com>
2024-12-24 16:23:36 -0700

    implemented the scan barcode feature

---
+++ src/scan.py
@@ -0,0 +1,32 @@
+import cv2
+
+def get_image():
+    detect_code = cv2.barcode.BarcodeDetector() 
+    capture = cv2.VideoCapture(0)
+    print("----Searching for Barcode----")
+
+    while True:
+        _, frame = capture.read()
+        result, decoded_info, _, _ = detect_code.detectAndDecode(frame)
+
+        if result:
+            print(f"Barcode: {decoded_info[0]}")
+        
+        cv2.imshow("Barcode Scanner", frame)
+
+        if cv2.waitKey(1) = ord('q'):
+            print("Scan Cancelled")
+            result = 'Cancelled'
+            break
+
+    capture.release()
+    cv2.destroyAllWindows()
+
+    return result
+
+def main():
+    get_result = get_image()
+    print(f"Result: {get_result}")
+
+
+
